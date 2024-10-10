15d2c56f8b49d13f046eef6af5c2a119217c2585
lini07003 <lini07003@E-PW05C37N>
2024-10-10 12:04:15 +0200

    Add project files.

---
+++ InlämningsUpg 2.3 NY/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace inlämnings_upg_2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("skriv hur många dagar du vill hyra bilen: ");
+            int dagar = int.Parse(Console.ReadLine());
+
+            Console.Write("Skriv hur månka kilometer du kommer köra: ");
+            int kilometer = int.Parse(Console.ReadLine());
+
+            int startavgift = 300;
+            int kostnadPerKilometer = kilometer;
+            int extraDagKostnad = (dagar - 1) * 500;
+
+            int totalKostnad = startavgift + kostnadPerKilometer + extraDagKostnad;
+
+            Console.WriteLine("Den totala kostnaden för är: " + totalKostnad + "kr");
+
+        }
+    }
+}
